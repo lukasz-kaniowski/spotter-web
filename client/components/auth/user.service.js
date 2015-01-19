1f06654a4c7deda226a42c2d14d1f9584e814c9b
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2015-01-19 23:17:45 +0000

    init

---
+++ client/components/auth/user.service.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('spotterWebApp')
+  .factory('User', function ($resource) {
+    return $resource('/api/users/:id/:controller', {
+      id: '@_id'
+    },
+    {
+      changePassword: {
+        method: 'PUT',
+        params: {
+          controller:'password'
+        }
+      },
+      get: {
+        method: 'GET',
+        params: {
+          id:'me'
+        }
+      }
+	  });
+  });
