b10ad8b5acb902cce4fd74c38027204235ef3c88
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2015-01-20 00:10:56 +0000

    test missions

--- server/api/mission/mission.spec.js
+++ server/api/mission/mission.spec.js
@@ -3,18 +3,29 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var Mission = require('./mission.model');
 
-describe('GET /api/missions', function() {
+describe('GET /api/missions', function () {
 
-  it('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/missions')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });
+  beforeEach(function (done) {
+    Mission.collection.remove(done)
+  });
+
+  it('should respond with JSON array', function (done) {
+    Mission.create({name: 'Some Mission'})
+      .then(function () {
+        request(app)
+          .get('/api/missions')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .end(function (err, res) {
+            if (err) return done(err);
+            res.body.should.be.instanceof(Array);
+            res.body.length.should.be.equal(1);
+            res.body[0].name.should.be.equal('Some Mission');
+            done();
+          });
+
+      })
   });
 });
